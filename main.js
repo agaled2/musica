9144243f9603608cc2ce71942ab0c1fbbf795737
agaled2 <130106607+agaled2@users.noreply.github.com>
2023-10-05 15:51:12 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
+volumen = 0.3;
+velocidad = 0.5;
+var manoDerX = 0;
+var manoDerY = 0;
+var puntosManoDer = 0;
+var manoIzqX = 0;
+var manoIzqY = 0;
+var puntosManoIzq = 0;
+
 function setup() {
     canvas = createCanvas(640, 480);//360,270
     background("green");
     video = createCapture(VIDEO);
     video.hide();
+    posnet = ml5.poseNet(video, listo);
+    posnet.on("pose", respuesta)
 }
 function draw() {
     translate(width, 0);
     scale(-1, 1)
     image(video, 0, 0, 640, 480);
+    if (puntosManoDer > 0.2) {
+        fill("red")
+        circle(manoDerX, manoDerY, 20);
+        ;
+    }
+    if (puntosManoIzq > 0.2) {
+        fill("blue")
+        circle(manoIzqX, manoDerY, 20);
+        ;
+    }
+
 }
 
 function preload() {
@@ -16,9 +38,28 @@ function preload() {
 }
 
 function reproducir() {
-   cancion.play(); 
+    if (!cancion.isPlaying()) {
+        cancion.play();
+        cancion.setVolume(volumen)
+        cancion.rate(velocidad);
+    }
 }
 
-function detener() {
-    cancion.stop(); 
+function stop() {
+    cancion.stop();
+}
+
+function listo() {
+    console.log("poseNet esta lista")
+}
+
+function respuesta(resultados) {
+    if (resultados && resultados.length > 0) {
+        manoDerX = resultados[0].pose.rigthWrist.x;
+        manoDerY = resultados[0].pose.rigthWrist.y;
+        puntosManoDer = resultados[0].pose.keypoints[10].score;
+        manoIzqX = resultados[0].pose.leftWrist.x;
+        manoIzqY = resultados[0].pose.leftWrist.y;
+        puntosManoIzq = resultados[0].pose.keypoints[9].score;
+    }
 }
